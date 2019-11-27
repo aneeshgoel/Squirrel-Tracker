525478189811e2865608ced87449a55b6c4682f1
Aneesh Goel <ag4182@columbia.edu>
2019-11-27 18:12:26 +0000

    starting the models file

--- squirrel_tracker/models.py
+++ squirrel_tracker/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
+from django.utils.translation import gettext as _
+
 # Create your models here.
+
+class Squirrel(models.Model):
+
+    TRUE = 'TRUE'
+    FALSE = 'FALSE'
+
+    X = models.DecimalField(
+            help_text=_('Longitutde of sighting'),
+            max_length=10,
+            max_digits=2,
+            decimal_places=5,
+        )
+
+    Y = models.DecimalField(
+            help_text=_('Latitude of sighting'),
+            max_length=10,
+            max_digits=2,
+            decimal_places=5,
+        )
+    
+    Hectare = models.CharField(
+            help_text=_('ID tag; derived from hectare grid'),
+            max_length=5,
+        )
+
+
+
+            
+            
