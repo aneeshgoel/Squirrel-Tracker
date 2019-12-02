058f184f597aec8e1c2e8e74ee3c511b5dcc33c1
Aneesh Goel <ag4182@columbia.edu>
2019-12-02 03:32:33 +0000

    pushing changes to github

--- squirrel_tracker/views.py
+++ squirrel_tracker/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here
 
 from django.http import HttpResponse
 
+from .models import Squirrel 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the squirrel tracker  index.")
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the squirrel tracker  index.")
+
+def 
